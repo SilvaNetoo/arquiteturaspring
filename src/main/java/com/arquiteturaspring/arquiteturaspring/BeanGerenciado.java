ca9d653e806da279ed35a4b34c2fc6bcf1f8fe3c
SilvaNetoo <silva.neto.sistemas@gmail.com>
2025-12-17 12:27:44 -0300

    Aprofundamento em injeção de dependencias

--- src/main/java/com/arquiteturaspring/arquiteturaspring/BeanGerenciado.java
+++ src/main/java/com/arquiteturaspring/arquiteturaspring/BeanGerenciado.java
@@ -1,4 +1,28 @@
 package com.arquiteturaspring.arquiteturaspring;
 
+import com.arquiteturaspring.arquiteturaspring.todos.TodoEntity;
+import com.arquiteturaspring.arquiteturaspring.todos.TodoValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class BeanGerenciado {
+
+    @Autowired
+    private TodoValidator validator;
+
+    public BeanGerenciado(TodoValidator validator) {
+        this.validator = validator;
+    }
+
+    public void atualizar () {
+        var todo = new TodoEntity();
+        validator.validar(todo);
+    }
+
+    @Autowired
+    public void setValidator(TodoValidator validator) {
+        this.validator = validator;
+    }
+
 }
--- src/main/java/com/arquiteturaspring/arquiteturaspring/ExemploInjecaoDependencia.java
+++ src/main/java/com/arquiteturaspring/arquiteturaspring/ExemploInjecaoDependencia.java
@@ -1,4 +1,30 @@
 package com.arquiteturaspring.arquiteturaspring;
 
+import com.arquiteturaspring.arquiteturaspring.todos.*;
+import jakarta.persistence.EntityManager;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class ExemploInjecaoDependencia {
+    public static void main (String[] args) throws SQLException {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("url");
+        dataSource.setUsername("user");
+        dataSource.setPassword("password");
+
+        Connection connection = dataSource.getConnection();
+
+        EntityManager entityManager = null;
+
+        TodoRepository repository = null; // new SimpleJpaRepository<TodoEntity, Integer>();
+        TodoValidator todoValidator = new TodoValidator(repository);
+        MailSender mailSender = new MailSender();
+
+        TodoService todoService = new TodoService(repository, todoValidator, mailSender);
+
+    }
 }
